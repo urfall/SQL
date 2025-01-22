6c7b040260a231c4cd088d035a0328cabef1f8b4
G1 <xxurfall@gmail.com>
2025-01-22 20:39:54 +0900

    Update jiwon_Week_1_problem_2.sql

--- PCSQL/jiwon_Week_1_problem_2.sql
+++ PCSQL/jiwon_Week_1_problem_2.sql
@@ -1,4 +1,25 @@
 -- 프로그래머스, 멸종위기의 대장균 찾기
 -- https://school.programmers.co.kr/learn/courses/30/lessons/301651
 
-SELECT 
+WITH RECURSIVE GEN AS (
+    SELECT ID, PARENT_ID, 1 GENERATION
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL 
+    
+    UNION ALL
+    
+    SELECT D.ID, D.PARENT_ID, GENERATION + 1
+    FROM ECOLI_DATA D JOIN GEN G 
+    ON G.ID = D.PARENT_ID 
+),
+GEN2 AS (
+    SELECT ID, PARENT_ID, GENERATION, (SELECT COUNT(*)
+                                       FROM GEN  D
+                                       WHERE G.ID = D.PARENT_ID) CHILD_COUNT
+    FROM GEN G
+)
+SELECT COUNT(*) COUNT, GENERATION 
+FROM GEN2
+WHERE CHILD_COUNT = 0
+GROUP BY GENERATION 
+ORDER BY GENERATION;
