d995fbe9c1941a834216f90bed3209e66f4edc1a
G1 <xxurfall@gmail.com>
2025-01-22 17:35:20 +0900

    Update jiwon_Week_1_problem_1.sql

--- PCSQL/jiwon_Week_1_problem_1.sql
+++ PCSQL/jiwon_Week_1_problem_1.sql
@@ -1,5 +1,5 @@
 -- 프로그래머스, 서울에 위치한 식당 목록 출력하기
--- https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=mysql
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131118
 
 SELECT REST_INFO.REST_ID, REST_INFO.REST_NAME, REST_INFO.FOOD_TYPE, REST_INFO.FAVORITES, REST_INFO.ADDRESS, ROUND(AVG(REST_REVIEW.REVIEW_SCORE), 2) AS SCORE
 FROM REST_INFO JOIN REST_REVIEW
